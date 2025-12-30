cc127481856f6e34df7c3560c64502e381b446bd
mldjordje <165197022+mldjordje@users.noreply.github.com>
2025-12-30 12:05:39 +0100

    Update booking success message

--- components/BookingForm.tsx
+++ components/BookingForm.tsx
@@ -578,7 +578,7 @@ export default function BookingForm() {
 
       setStatus({
         type: "success",
-        message: "Termin je poslat! Javljamo potvrdu uskoro.",
+        message: "Vas termin je potvrdjen.",
       });
       setLastBooked({
         serviceName: selectedService.name,
